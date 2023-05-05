97e8a6182e41ab313b2aaeaa5fa40a2cdefe332f
aikaterna <20862007+aikaterna@users.noreply.github.com>
2023-05-04 21:12:33 -0700

    Discontinued Red 3.4.x cogs

---
+++ blurplefy/__init__.py
@@ -0,0 +1,7 @@
+from .blurplefy import Blurplefy
+
+__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
+
+
+async def setup(bot):
+    await bot.add_cog(Blurplefy(bot))
